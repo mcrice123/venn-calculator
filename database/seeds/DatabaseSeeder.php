30c73455ae67d8deddcd8005325f2a53a6663afb
Maria Rice <maria@lifeboatcreative.com>
2020-05-14 15:50:12 -0500

    Add migrations, models, and seeders for tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         $this->call(UserSeeder::class);
+         $this->call(TeamSeeder::class);
+         $this->call(TeamItemSeeder::class);
+         $this->call(SupplierSeeder::class);
+         $this->call(SupplierItemSeeder::class);
+         $this->call(ProSeeder::class);
+         $this->call(ItemSeeder::class);
+         $this->call(ConSeeder::class);
     }
 }
