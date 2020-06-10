30407e6025975789042573b88c7a07d7fd0c358f
Maria Rice <maria@lifeboatcreative.com>
2020-06-10 15:32:04 -0500

    Add user controller methods

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function suppliers() {
+        return $this->hasMany('App\Supplier');
+    }
+
+    public function items() {
+        return $this->hasMany('App\Supplier')->hasMany('App\SupplierItem')->belongsTo('App\Item');
+    }
 }
